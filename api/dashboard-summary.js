032a7fa05c81601c132d19ea4bf18ff96b6b0bef
pdberon <73356763+pdberon@users.noreply.github.com>
2026-06-09 12:24:21 -0300

    Add dashboard summary data for tournament statistics

---
+++ api/dashboard-summary.js
@@ -0,0 +1,17 @@
+{
+  "tournamentDay": 12,
+  "stage": "GROUP STAGE",
+  "todayMatches": [
+    "United States vs Paraguay",
+    "Canada vs Bosnia-Herzegovina"
+  ],
+
+  "totalEngagement": 123456789,
+  "totalPosts": 45678,
+  "totalVideoViews": 987654321,
+  "totalPageViews": 45678901,
+  "avgUniqueVisitors": 234567,
+  "totalWatchTime": 345678,
+  "totalContentStarts": 12345678,
+  "totalLiveViews": 456789
+}
