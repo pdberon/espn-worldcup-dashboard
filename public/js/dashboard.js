000b8464a576c7f84cb56edfaf02710456bfec8f
pdberon <73356763+pdberon@users.noreply.github.com>
2026-06-10 16:39:42 -0300

    Add date input click event to show date picker

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -427,3 +427,27 @@ document
     link.click();
 
 });
+
+
+document
+.querySelectorAll(
+    'input[type="date"]'
+)
+.forEach(input => {
+
+    input.addEventListener(
+        "click",
+        () => {
+
+            if(
+                input.showPicker
+            ){
+
+                input.showPicker();
+
+            }
+
+        }
+    );
+
+});
